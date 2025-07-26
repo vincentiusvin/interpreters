21c8d4d7f55d09e75d30508b94962571f12c014f
vincentiusvin <vincentius_vin@yahoo.com>
2025-07-26 21:34:50 +0700

    feat: repl and script mode

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("Usage is jlox [script]")
-	} else {
+	argLen := len(os.Args)
+	if argLen == 1 {
+		runPrompt()
+	} else if argLen == 2 {
 		runFile(os.Args[1])
+	} else {
+		panic("Usage is jlox [script]")
 	}
 }
 
-func runFile(file string) {
-	fmt.Println(file)
+func runFile(fileName string) error {
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	run(string(file))
+	return nil
+}
+
+func runPrompt() error {
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		line := sc.Text()
+		run(line)
+	}
+
+	return nil
+}
+
+func run(line string) {
+	fmt.Println(line)
 }
